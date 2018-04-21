98f7cfd103447548c4a52bdb67203ceaf9f03e08
Bala Krishna <prince.balakrishna@gmail.com>
2018-04-21 07:52:03 +0530

    Changes in Questions

--- app/src/main/java/com/koffeecuptales/feedify/QuestionSet.java
+++ app/src/main/java/com/koffeecuptales/feedify/QuestionSet.java
@@ -3,10 +3,11 @@ package com.koffeecuptales.feedify;
 class QuestionSet {
 
     private final String[] questions = {
-            "Level of Topics covered?",
-            "Quality of food provided?",
-            "Air Quality in the room?",
-            "Level of contents in the lecture and their presentation?"
+            "Presentation skills of the speaker",
+            "Question session",
+            "Faculty",
+            "Content of the course",
+            "Relevance of the Topic"
     };
 
     public String getQuestion(int option) {
